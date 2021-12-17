2d2dfaad4d2ac608a8cbd67c3f364a0cc7e7a6a2
slice <tinyslices@gmail.com>
2021-12-17 13:07:03 -0800

    Publish new files to a Discord webhook

--- Carnage/Carnage/Embeds.hs
+++ Carnage/Carnage/Embeds.hs
@@ -7,16 +7,19 @@ import qualified Data.Aeson as AE
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format.ISO8601 (iso8601Show)
 
+-- | Returns a link to a Dbree search result.
+searchResultUrl :: DbreeSearchResult -> Text
+searchResultUrl DbreeSearchResult {uploadID = DbreeID uploadID} =
+  dbreeRootURL <> "/v/" <> uploadID
+
 -- | Encodes a 'DbreeSearchResult' into a Discord embed as JSON.
 encodeSearchResultEmbed :: DbreeSearchResult -> IO AE.Value
-encodeSearchResultEmbed DbreeSearchResult {fileName, fileSize, uploadID = DbreeID uploadID} = do
+encodeSearchResultEmbed sr@DbreeSearchResult {fileName, fileSize} = do
   time <- getCurrentTime
   pure $
     AE.object
       [ "title" .= fileName,
-        "url" .= fileURL,
+        "url" .= searchResultUrl sr,
         "timestamp" .= iso8601Show time,
         ("footer", AE.object ["text" .= fileSize])
       ]
-  where
-    fileURL = dbreeRootURL <> "/v/" <> uploadID
--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,11 @@ module Main where
 import Carnage
 import Carnage.Discovery
 import Control.Lens ((.~), (?~))
-import Data.Aeson (FromJSON (..), eitherDecode', withObject, (.:))
+import Data.Aeson (FromJSON (..), Value, eitherDecode', object, withObject, (.:), (.=))
 import qualified Data.HashMap.Strict as HM
 import qualified Data.HashSet as HS
-import qualified Data.Time.Clock as T
+import qualified Data.Text as T
+import qualified Data.Time.Clock as C
 import Network.HTTP.Client (Cookie (..), createCookieJar)
 import qualified Network.Wreq as W
 import Polysemy
@@ -39,7 +40,8 @@ data Config = Config
     ddgidCookie :: Text,
     queries :: [Text],
     userAgent :: Text,
-    seenFilePath :: FilePath
+    seenFilePath :: FilePath,
+    discordWebhookUrl :: Text
   }
   deriving (Show)
 
@@ -52,30 +54,55 @@ instance FromJSON Config where
       <*> v .: "queries"
       <*> v .: "userAgent"
       <*> v .: "seenFilePath"
+      <*> v .: "discordWebhookUrl"
 
 readConfig :: FilePath -> IO Config
 readConfig path = do
   text <- readFileLBS path
   either fail pure $ eitherDecode' text
 
+postToWebhook :: Text -> Text -> [DbreeSearchResult] -> IO ()
+postToWebhook discordWebhookUrl query results = bodyIO >>= void . W.post (toString discordWebhookUrl)
+  where
+    plural = if length results == 1 then "" else "s"
+    header = "Detected " <> show (length results) <> " new file" <> plural <> " for query `" <> query <> "`."
+
+    markdownLink text url = "[" <> text <> "](" <> url <> ")"
+    markdownLinkWithoutEmbed text url = markdownLink text ("<" <> url <> ">")
+
+    compactListing =
+      unlines
+        . fmap
+          ( \sr@DbreeSearchResult {fileName, fileSize} ->
+              "\8226 "
+                <> markdownLinkWithoutEmbed fileName (searchResultUrl sr)
+                <> " ("
+                <> fileSize
+                <> ")"
+          )
+
+    truncateTo :: Int -> Text -> Text
+    truncateTo maxLength text =
+      if T.length text > maxLength
+        then T.take (maxLength - 1) text <> "\8230"
+        else text
+
+    bodyIO :: IO Value
+    bodyIO =
+      if length results > 10
+        then pure $ object ["content" .= truncateTo 2000 (header <> "\n\n" <> compactListing results)]
+        else (\embeds -> object ["content" .= header, "embeds" .= embeds]) <$> traverse encodeSearchResultEmbed results
+
 main :: IO ()
 main =
   do
     configFilePath <- getArgs >>= maybe (fail "missing config file path argument") pure . listToMaybe
-    Config
-      { ddg1Cookie,
-        ddg2Cookie,
-        ddgidCookie,
-        queries,
-        userAgent,
-        seenFilePath
-      } <-
-      readConfig configFilePath
+    Config {ddg1Cookie, ddg2Cookie, ddgidCookie, queries, userAgent, seenFilePath, discordWebhookUrl} <- readConfig configFilePath
 
     unlessM (doesFileExist seenFilePath) (writeFileBS seenFilePath "")
 
-    now <- T.getCurrentTime
-    let makeCookie name value = Cookie name value (T.addUTCTime (T.nominalDay * 30) now) "dbree.org" "/" now now True True False False
+    now <- C.getCurrentTime
+    let makeCookie name value = Cookie name value (C.addUTCTime (C.nominalDay * 30) now) "dbree.org" "/" now now True True False False
         cookieJar =
           createCookieJar
             [ makeCookie "__ddg1" $ encodeUtf8 ddg1Cookie,
@@ -86,18 +113,21 @@ main =
           W.defaults
             & W.header "User-Agent" .~ [encodeUtf8 userAgent]
             & W.cookies ?~ cookieJar
+
     queryResults <- runQueriesIO options seenFilePath queries
+
     traverse_
       ( \(query, files) ->
           putTextLn ("*** for query " <> show query <> ", discovered " <> show (length files) <> " file(s)")
-            >> traverse_
-              ( \DbreeSearchResult {fileName, fileSize} ->
-                  putTextLn $ "  - " <> fileName <> " (" <> fileSize <> ")"
-              )
-              files
+            >> traverse_ printSearchResult files
+            >> unless (null files) (postToWebhook discordWebhookUrl query files)
       )
       (HM.toList queryResults)
   where
+    printSearchResult :: DbreeSearchResult -> IO ()
+    printSearchResult DbreeSearchResult {fileName, fileSize} =
+      putTextLn $ "  - " <> fileName <> " (" <> fileSize <> ")"
+
     runQueriesIO :: W.Options -> FilePath -> [Text] -> IO (HashMap Text [DbreeSearchResult])
     runQueriesIO options path queries =
       processQueries queries
